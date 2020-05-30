3028466a251236664455b7e83b62f2d2f704e7f4
Gautam <gautam3491@gmail.com>
2020-05-30 13:12:36 +1200

    commit check

--- app/src/main/java/com/example/comp709c3/MainActivity.java
+++ app/src/main/java/com/example/comp709c3/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.comp709c3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
