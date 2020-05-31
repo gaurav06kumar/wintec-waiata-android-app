5b9752ca640a49c1e63bd7a1534c1118f1536d79
Gautam <gautam3491@gmail.com>
2020-05-31 14:14:11 +1200

    detail page data view

--- app/src/main/java/com/example/comp709c3/DetailActivity.java
+++ app/src/main/java/com/example/comp709c3/DetailActivity.java
@@ -2,13 +2,22 @@ package com.example.comp709c3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    String name;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        textView = findViewById(R.id.name);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        textView.setText(name.toString());
     }
 }
