54d39aa51394f567c31e225611d8cc35cf1ec0fa
gaurav <gauravkumar5823@gmail.com>
2020-05-31 14:27:14 +1200

    video player is done

--- app/src/main/java/com/example/comp709c3/DetailActivity.java
+++ app/src/main/java/com/example/comp709c3/DetailActivity.java
@@ -3,8 +3,11 @@ package com.example.comp709c3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class DetailActivity extends AppCompatActivity {
     String name;
@@ -14,10 +17,19 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        textView = findViewById(R.id.name);
+
 
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
-        textView.setText(name.toString());
+
+        VideoView videoView=findViewById(R.id.video_view);
+        String videoPath="android.resource://"+getPackageName() + "/" + R.raw.ekorekoe_1;
+        Uri uri=Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController=new MediaController(this);  //buttons
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);       //position on screen
+
     }
 }
