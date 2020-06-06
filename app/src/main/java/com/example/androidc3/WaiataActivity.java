8cb73c88bf21620d1193d8a0eee72227e2bc9b7d
gautam3491 <47635591+gautam3491@users.noreply.github.com>
2020-06-06 16:03:51 +1200

    about us and grid list view

--- app/src/main/java/com/example/androidc3/WaiataActivity.java
+++ app/src/main/java/com/example/androidc3/WaiataActivity.java
@@ -1,8 +1,17 @@
 package com.example.androidc3;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -18,11 +27,16 @@ public class WaiataActivity extends AppCompatActivity {
     private ArrayList<WaiataModel> charList = new ArrayList<>();
     private GridOfCharAdapter gridOfCharAdapter;
     private RecyclerView charRecyclerview;
+    private Toolbar toolbar;
+    Boolean swapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiata);
 
+        swapView = true;
+
+        //toolbar = findViewById(R.id.toolbarList);
         //store the value in the form of array
         WaiataModel charModel0 = new WaiataModel(1,R.drawable.ekorekoeengaro, "E Kore Koe E Ngaro", "Zeus, the Lord of Heaven, is a ranged intelligence hero who functions almost solely as a nuker. He is usually played as a semi-carry ganker type hero, who instead of utilizing disables, focuses solely on delivering tremendous amounts of magical damage to his foes.", "Electric Shock","Attack + defence", R.raw.ekorekoe_1 );
         charList.add(charModel0);
@@ -33,18 +47,59 @@ public class WaiataActivity extends AppCompatActivity {
         charRecyclerview = findViewById(R.id.recyclerview);
 
         //        for grid
-        charRecyclerview.setLayoutManager(new GridLayoutManager(this, 2));
+        charRecyclerview.setLayoutManager(new GridLayoutManager(this, 1));
         charRecyclerview.setItemAnimator(new DefaultItemAnimator());
         charRecyclerview.setAdapter(gridOfCharAdapter);
 
         //Toolbar
-        Toolbar toolbarList = findViewById(R.id.toolbarList);
-        toolbarList.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-//                CustomIntent.customType(view.getContext(), "right-to-left");
-            }
-        });
+//        Toolbar toolbarList = findViewById(R.id.toolbarList);
+//        toolbarList.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+////                CustomIntent.customType(view.getContext(), "right-to-left");
+//            }
+//        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_topmenu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemlist:
+                Toast.makeText(this, "item list selected", Toast.LENGTH_SHORT).show();
+                swapView(R.id.itemlist, swapView, item);
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "item 3 selected", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, AboutUsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void swapView(int id, boolean swapView, MenuItem item){
+        if(swapView == true){
+            swapView = false;
+            item.setIcon(R.drawable.ic_grid);
+        }else if (swapView == false){
+            swapView = true;
+            item.setIcon(R.drawable.ic_list);
+        }
+
+    }
+
+
 }
