965bfac2859b147140da74193eea0b5f4d6f23bd
gaurav <gauravkumar5823@gmail.com>
2020-06-06 17:45:04 +1200

    splash screen with progress bar

---
+++ app/src/main/java/com/example/androidc3/Splash.java
@@ -0,0 +1,54 @@
+package com.example.androidc3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+    private Timer timer;
+    private ProgressBar progressBar;
+    private int i=0;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+
+
+
+        final long period = 100;
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //this repeats every 100 ms
+                if (i<100){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    });
+                    progressBar.setProgress(i);
+                    i+=4;
+                }else{
+                    //closing the timer
+                    timer.cancel();
+                    Intent intent =new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                    // close this activity
+                    finish();
+                }
+            }
+        }, 0, period);
+    }
+}
